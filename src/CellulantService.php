5c29be4064f1cb57172ca139302beb996a16d816
Alpha Olomi <alphaolomi@gmail.com>
2023-07-14 17:25:23 +0300

    Update CellulantService.php

--- src/CellulantService.php
+++ src/CellulantService.php
@@ -9,12 +9,73 @@ namespace Alphaolomi\Cellulant;
  *
  * @package Alphaolomi\Cellulant
  */
-class CellulantService
-{
-    // 🚀 Build wonderful things
+class CellulantService {
+    private $baseUrl;
+    private $authenticationToken;
 
-    public function __construct()
-    {
-        // ... 📡
+    public function __construct($baseUrl) {
+        $this->baseUrl = $baseUrl;
+        $this->authenticationToken = null;
+    }
+
+    public function authenticate() {
+        // Retrieve an authentication token
+        // Make a request to the authentication API
+        // Store the authentication token for future requests
+    }
+
+    public function initiateCheckout() {
+        // Initiate a checkout request
+        // Make a request to the checkout API
+    }
+
+    public function postCharge() {
+        // Post a charge request
+        // Make a request to the charge API
+    }
+
+    public function initiateCombinedCheckoutCharge() {
+        // Initiate a combined checkout-charge request
+        // Make a request to the combined checkout-charge API
+    }
+
+    public function handleWebhook() {
+        // Handle the server-to-server webhook call
+        // Perform necessary actions after a payment is completed
+    }
+
+    public function acknowledgeTransaction() {
+        // Acknowledge a transaction
+        // Make a request to the acknowledgement API
+    }
+
+    public function refundPayment() {
+        // Refund a payment
+        // Make a request to the refund API
+    }
+
+    public function queryCheckoutStatus() {
+        // Query the status of a checkout request
+        // Make a request to the query status API
+    }
+
+    public function processDirectCardPayment() {
+        // Process a direct card payment
+        // Make a request to the direct card API
+    }
+
+    public function validateOTP() {
+        // Validate OTP (One-Time Password)
+        // Make a request to the OTP validation API
+    }
+
+    public function generateCardToken() {
+        // Generate a token for card details
+        // Make a request to the card tokenization API
+    }
+
+    public function fetchForexExchangeRate() {
+        // Fetch the current forex exchange rate
+        // Make a request to the fetch forex API
     }
 }
