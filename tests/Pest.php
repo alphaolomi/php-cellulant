9788e8d0fe7229a80e9f89c2b4e5e3c3ecf56c43
Alpha Olomi <alphaolomi@gmail.com>
2023-07-15 03:48:07 +0300

    Update Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,3 +1,27 @@
 <?php
 
 // ...
+function mockGuzzleClient()
+{
+    $mock = new \GuzzleHttp\Handler\MockHandler();
+    $handler = \GuzzleHttp\HandlerStack::create($mock);
+    $client = new \GuzzleHttp\Client(['handler' => $handler]);
+    return $client;
+}
+
+function mockGuzzleClientWithResponse($response)
+{
+    $mock = new \GuzzleHttp\Handler\MockHandler([
+        new \GuzzleHttp\Psr7\Response(200, [], $response),
+    ]);
+    $handler = \GuzzleHttp\HandlerStack::create($mock);
+    $client = new \GuzzleHttp\Client(['handler' => $handler]);
+    return $client;
+}
+
+// skip this test if the environment variable is not set
+// wip
+function skipIfNotSet($env)
+{
+    return fn () => getenv($env) === null;
+}
